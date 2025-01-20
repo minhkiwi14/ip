d8f1f6cdd99b7a85b8abf15229c2c1dd184716cf
minhkiwi14 <minhnn1472005@gmail.com>
2025-01-20 21:45:02 +0700

    Add support for add and list

--- src/main/java/Kiwi.java
+++ src/main/java/Kiwi.java
@@ -1,6 +1,20 @@
 import java.util.Scanner;
 
 public class Kiwi {
+    /**
+     * Prints each item in the provided array with numbering, indented by 4 spaces.
+     * After printing all items, it prints a separator line.
+     *
+     * @param array The array of strings.
+     * @param numOfItems The number of items in the array.
+     */
+    public static void printList(String[] array, int numOfItems) {
+        for (int i = 0; i < numOfItems; i++) {
+            System.out.println("    " + (i + 1) + ". " + array[i]);
+        }
+        System.out.println("    ______________________________");
+    }
+
     public static void main(String[] args) {
         String logo = " _   __  _              _    __     __\n"
                 + "| | / / |_|            |_|  /  \\   /  \\\n"
@@ -18,6 +32,9 @@ public class Kiwi {
         Scanner scanner = new Scanner(System.in);
         String userInput = "";
 
+        String[] taskList = new String[100];
+        int numOfItems = 0;
+
         while (scanner.hasNextLine()) {
             userInput = scanner.nextLine();
             System.out.println("    ______________________________");
@@ -27,8 +44,14 @@ public class Kiwi {
                 break;
             }
 
-            System.out.println(userInput);
-            System.out.println("    ______________________________");
+            if (userInput.equals("list")) {
+                printList(taskList, numOfItems);
+            } else {
+                taskList[numOfItems] = userInput;
+                numOfItems++;
+                System.out.println("    added: " + userInput);
+                System.out.println("    ______________________________");
+            }
         }
 
         scanner.close();
