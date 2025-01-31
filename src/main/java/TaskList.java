fc271bf59aacbd42ecbc6ae78770b4e92ff5c274
minhkiwi14 <minhnn1472005@gmail.com>
2025-01-31 22:11:50 +0700

    Implement class TaskList

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,2 +1,51 @@
+import java.util.ArrayList;
+
 public class TaskList {
+    private final ArrayList<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public Task deleteTask(int index) throws KiwiException {
+        try {
+            return tasks.remove(index);
+        } catch (IndexOutOfBoundsException e) {
+            throw new KiwiException("Invalid task number!");
+        }
+    }
+
+    public Task getTask(int index) throws KiwiException {
+        try {
+            return tasks.get(index);
+        } catch (IndexOutOfBoundsException e) {
+            throw new KiwiException("Invalid task number!");
+        }
+    }
+
+    public void markTask(int index) throws KiwiException {
+        Task task = getTask(index);
+        task.markAsDone();
+    }
+
+    public void unmarkTask(int index) throws KiwiException {
+        Task task = getTask(index);
+        task.markAsUndone();
+    }
+
+    public int size() {
+        return tasks.size();
+    }
+
+    public ArrayList<Task> getAllTasks() {
+        return tasks;
+    }
 }
